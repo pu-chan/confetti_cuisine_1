44c7256504ca462ffdec86a78e74361f90e79559
mamorita <mamorita@solxyz.co.jp>
2020-04-23 20:59:39 +0900

    12-6 動的コンテンツをビューに渡す

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,8 +1,24 @@
 "use strict";
 
+// コース内容を変数に代入してビューにわたす
+const courses = [
+    {
+        title: "Event Driven Cakes",
+        cost: 50
+    },
+    {
+        title: "Asynchronous Artichoke",
+        cost: 25
+    },
+    {
+        title: "Object Oriented Orange Juice",
+        cost: 10
+    }
+];
+
 // 経路ごとのコールバック関数
 exports.showCourses = (req, res) => {
-    res.render("courses");
+    res.render("courses", {offeredCourses: courses});
 };
 exports.showSignUp = (req, res) => {
     res.render("contact");
